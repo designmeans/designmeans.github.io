85a74b0c2bcc90ff24640e2c58456307b54bd17d
Erich Nagler <erich@designmeans.com>
2016-10-27 10:33:02 -0400

    Classwork

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,28 @@
+document.getElementById('title').style.color = 'red';
+
+document.querySelector('#text-chunk').style.color = 'blue';
+
+
+
+// document.write('Writing to the document!');
+
+// document.write('<h1>' + 10 * 10 + '</h1>');
+
+// document.write('<h2>' + 100 / 10 + '<h2>');
+
+// document.write('<p>' + (50 + 17) + '<p>');
+
+// var first = 'Erich'
+// var last = 'Nagler'
+
+// document.write(`My name is ${first} ${last}.`);
+
+// function hello () {
+// 	var greeting = 'hello!';
+
+// 	greeting = 'hello, world!';
+
+// 	alert(greeting);
+// }
+
+// hello();
